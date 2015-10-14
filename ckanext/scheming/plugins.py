afe1648e583416b185499151f834ad444a35a5c6
Marc Fortier <Marc.Fortier@statcan.gc.ca>
2015-10-14 15:11:01 -0400

    somehow the declaration of the isodatetime validator got lost

--- ckanext/scheming/plugins.py
+++ ckanext/scheming/plugins.py
@@ -90,6 +90,7 @@ class _SchemingMixin(object):
             'scheming_required': scheming_required,
             'scheming_multiple_choice': scheming_multiple_choice,
             'scheming_multiple_choice_output': scheming_multiple_choice_output,
+            'scheming_isodatetime': scheming_isodatetime,
             'convert_to_json_if_date': convert_to_json_if_date,
             }
 
